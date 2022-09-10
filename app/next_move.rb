3cec41325112669f62a9f9c7d18f02ac7a312865
Sully Birashk <sully.birashk@gmail.com>
2022-09-09 20:04:09 -0600

    Feat: Validation method to confirm r is a possible number

--- app/next_move.rb
+++ app/next_move.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-def error_handling(n, grid_array)
+def error_handling(n, r, c, grid_array)
     number_confirmation = (n.odd? && 3 <= n && n < 100)
 
     grid_confirmation = (n == grid_array.size)
@@ -9,14 +9,16 @@ def error_handling(n, grid_array)
         return 'Error: Input Needs to be an Odd Number Between 3 - 99'
     elsif number_confirmation && grid_confirmation == false
         return "Grid Doesn't Match n x n Format"
+    elsif (r <= (n - 1)) == false
+        return "Error: Row input is incorrect"
     end
 
 end
 
 def nextMove(n, r, c, grid)
 
-    if error_handling(n, grid) != nil
-        return error_handling(n, grid)
+    if error_handling(n, r, c, grid) != nil
+        return error_handling(n, r, c, grid)
     end
 
 end
