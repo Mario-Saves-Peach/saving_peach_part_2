a3b5f8d4c90263f64cdf8a0020c11ea1c315696a
Sully Birashk <sully.birashk@gmail.com>
2022-09-09 20:28:11 -0600

    Refactor: Change validation to  match web challenge requirements

--- app/next_move.rb
+++ app/next_move.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def error_handling(n, r, c, grid_array)
-    number_confirmation = (n.odd? && 3 <= n && n < 100)
+    number_confirmation = (2 <= n && n < 100)
 
     grid_confirmation = (n == grid_array.size)
 
     if number_confirmation == false
-        return 'Error: Input Needs to be an Odd Number Between 3 - 99'
+        return 'Error: Input Needs to be a Number Between 2 - 99'
     elsif number_confirmation && grid_confirmation == false
         return "Grid Doesn't Match n x n Format"
     elsif (r <= (n - 1)) == false
