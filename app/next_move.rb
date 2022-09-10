63efba48af778b442833bfaab795aeaa47905f78
Sully Birashk <sully.birashk@gmail.com>
2022-09-09 19:54:41 -0600

    Feat: Validation method to confirm grid is (n x n) format

--- app/next_move.rb
+++ app/next_move.rb
@@ -3,8 +3,12 @@ require 'pry'
 def error_handling(n, grid_array)
     number_confirmation = (n.odd? && 3 <= n && n < 100)
 
+    grid_confirmation = (n == grid_array.size)
+
     if number_confirmation == false
         return 'Error: Input Needs to be an Odd Number Between 3 - 99'
+    elsif number_confirmation && grid_confirmation == false
+        return "Grid Doesn't Match n x n Format"
     end
 
 end
