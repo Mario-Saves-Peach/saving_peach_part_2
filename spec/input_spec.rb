816def6d183522b3e52db6dbcaa88dce2866b40c
Sully Birashk <sully.birashk@gmail.com>
2022-09-09 19:31:49 -0600

    Test: Validating n input

---
+++ spec/input_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'pry'
+# require './app/next_move.rb'
+
+RSpec.describe 'nextMove(n, r, c, grid) Error Handling' do
+    before :each do
+        @n = 5
+        @r = 2
+        @c = 3
+        @grid = '----------p-m------------'
+    end
+
+    describe 'number input errors' do
+        it 'returns error if n is not an odd number from 3 - 99' do
+            n = 4
+
+            response = nextMove(n, @r, @c, @grid)
+
+            expect(response).to eq('Error: Input Needs to be an Odd Number Between 3 - 99')
+
+            n = 101
+
+            response = nextMove(n, @r, @c, @grid)
+
+            expect(response).to eq('Error: Input Needs to be an Odd Number Between 3 - 99')
+        end
+    end
+end
