7c6b471cb10c452b9a710219e0772ebc83cf45ab
Sully Birashk <sully.birashk@gmail.com>
2022-09-21 15:07:35 -0700

    Test: Refactor test to expect integers instead of strings

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -13,13 +13,13 @@ RSpec.describe 'nextMove(n, r, c, grid) Output' do
     it 'confirm marios position' do
         response = findMario(@grid)
 
-        expect(response).to eq({:row=>"2", :column=>"3"})
+        expect(response).to eq({:row=>2, :column=>3})
     end
 
     it 'locate Peaches position' do
         response = findPeach(@grid)
 
-        expect(response).to eq({:row=>"2", :column=>"0"})
+        expect(response).to eq({:row=>2, :column=>0})
     end
 
     it 'shows mario the next move to Peach!' do
